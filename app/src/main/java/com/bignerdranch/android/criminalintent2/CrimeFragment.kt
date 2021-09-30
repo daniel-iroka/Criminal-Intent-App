dbd9bfdde228383464336f59069f81bf32965975
daniel-iroka <danieliroka67@gmail.com>
2021-09-30 21:23:52 +0100

    added ViewHolder and RecyclerView.Adapter to set what the RecyclerView will display

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
@@ -12,7 +12,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 // This is our Fragment which we will use to work on our Fragment's view
-// THIS FILE WILL DETAIL OUR CRIMES DETAIL
+// THIS FILE WILL CONTAIN OUR CRIME'S DETAIL
 class CrimeFragment : Fragment() {
 
     private lateinit var crime :Crime
--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,7 +13,10 @@ import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
-// TODO WHEN I COME BACK, I WILL GO TO "Creating an Item View Layout"
+
+// TODO CONTINUE FROM FIGURE 9.9 PAGE 187...
+// TODO WHEN I COME BACK IN THE EVENING, GO TO PRACTICE AND READ MORE ON RECYCLER VIEW.
+
 
 // This is the fragment to display our crime list objects
 // THIS FILE WILL BE THE FRAGMENT TO DISPLAY OUR CRIME LIST
@@ -46,11 +50,49 @@ class CrimeListFragment : Fragment() {
         //  finding views by their IDs
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
 
-        // A recycler view needs a LayoutManger to work. It used to position items on the screen itself
+        // A recycler view needs a LayoutManger to work. It is used to position items on the screen itself
         // The linearLayoutManager will position the items in the list vertically
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
         return view
+    }
+
+
+    // This class is where we wire up views in our item list with a ViewHolder
+    // A ViewHolder in a RecyclerView makes reference to a view by their "itemViews" or storing them in a property called itemView
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+
+            val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
+            val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+
+        }
+
+
+    // A recyclerView does not create or hold ViewHolders by itself, rather it asks an "Adapter" to do so
+    // This class here sets the data the Recycler view will display through the CrimeHolder
+    private inner class CrimeAdapter(var crimes: List<Crime>)
+        : RecyclerView.Adapter<CrimeHolder>() {
+
+        // This wraps up the inflated recyclerView layout and passes to the CrimeHolder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun getItemCount() = crimes.size   // This reveals the number of items in the list of crimes
+
+
+        // This obtains crimes from a particular position from the crime list and passes it to the CrimeHolder
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title             //Then here the crime holder then sets the title and date for that particular crime
+                dateTextView.text = crime.date.toString()
+            }
+        }
+
+
     }
 
 
