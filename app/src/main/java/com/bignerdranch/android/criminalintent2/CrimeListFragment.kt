054ea67679cdc10ea2ae3250a3dc60e6456c25ca
daniel-iroka <danieliroka67@gmail.com>
2021-10-06 17:15:31 +0100

    added a function to connect our adapter to our RecyclerView as well as implemented an onClickListener to display a Toast message when a crime is clicked

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,15 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 private const val TAG = "CrimeListFragment"
 
 
-// TODO CONTINUE FROM FIGURE 9.9 PAGE 187...
-// TODO WHEN I COME BACK IN THE EVENING, GO TO PRACTICE AND READ MORE ON RECYCLER VIEW.
-
 
 // This is the fragment to display our crime list objects
 // THIS FILE WILL BE THE FRAGMENT TO DISPLAY OUR CRIME LIST
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView : RecyclerView
+    private var adapter : CrimeAdapter? = null
 
 
     // We set a ViewModelProvider to provide and instance of CrimeListViewModel and return it whenever the OS requests for a new one.
@@ -54,19 +53,59 @@ class CrimeListFragment : Fragment() {
         // The linearLayoutManager will position the items in the list vertically
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
+        updateUI()
+
         return view
+    }
+
+
+    // This is a function that connects our adapter to our RecyclerView and populates our UI
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
     }
 
 
     // This class is where we wire up views in our item list with a ViewHolder
     // A ViewHolder in a RecyclerView makes reference to a view by their "itemViews" or storing them in a property called itemView
     private inner class CrimeHolder(view: View)
-        : RecyclerView.ViewHolder(view) {
+        : RecyclerView.ViewHolder(view), View.OnClickListener {
 
-            val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
-            val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+        private lateinit var crime : Crime
+
+            private val titleTextView : TextView = itemView.findViewById(R.id.crime_title)
+            private val dateTextView : TextView = itemView.findViewById(R.id.crime_date)
+
+
+        // We set an onClickListener on each crime represented by their itemViews
+        // The itemView is the view for the entire row
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+
+
+        // This function is added here so that our ViewHolder will do the binding work of the crimes instead of our TextViews(it is a good practice)
+        // Because the Adapter should know as little as possible the details of the ViewHolder
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+
+        // Since our ViewHolder implements the OnCLickListener itself, we need to implements its members,
+        // In this case, we need to set what will happen when our button is clicked
+        override fun onClick(v: View) {
+            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+        }
 
         }
+
+
+    /** || IMPORTANT NOTE : The "RecyclerView" does not set data by itself, rather it calls in the Adapter to do it. So  **/
+
 
 
     // A recyclerView does not create or hold ViewHolders by itself, rather it asks an "Adapter" to do so
@@ -74,7 +113,7 @@ class CrimeListFragment : Fragment() {
     private inner class CrimeAdapter(var crimes: List<Crime>)
         : RecyclerView.Adapter<CrimeHolder>() {
 
-        // This wraps up the inflated recyclerView layout and passes to the CrimeHolder
+        // This wraps up the inflated recyclerView layout and passes to the CrimeHolder creating a new ViewHolder
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
             val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
@@ -86,10 +125,8 @@ class CrimeListFragment : Fragment() {
         // This obtains crimes from a particular position from the crime list and passes it to the CrimeHolder
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title             //Then here the crime holder then sets the title and date for that particular crime
-                dateTextView.text = crime.date.toString()
-            }
+            holder.bind(crime)    // The onBindViewHolder() then passes a particular crime according to its position
+            // TODO GO THROUGH.... WHEN I COME BACK
         }
 
 
