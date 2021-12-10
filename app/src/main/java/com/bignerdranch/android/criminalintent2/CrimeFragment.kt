b5652ad81384ac9d291946f9148aee1386b93211
daniel-iroka <danieliroka67@gmail.com>
2021-12-10 22:56:24 +0100

    Added content Description attributes to some widgets to enable TalkBack read it out to Users

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
@@ -154,8 +154,10 @@ class CrimeFragment : Fragment()   {
         if (photoFile.exists()) {
             val bitmap = getScaledBitmap(photoFile.path, imageViewWidth, imageViewHeight)
             photoView.setImageBitmap(bitmap)
+            photoView.contentDescription = getString(R.string.crime_photo_image_description)
         }  else {
             photoView.setImageDrawable(null)
+            photoView.contentDescription = getString(R.string.crime_photo_no_image_description)
         }
     }
 
